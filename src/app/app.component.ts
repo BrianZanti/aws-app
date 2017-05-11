f29b302941d319ba45ce82571f5ba1fc65f60381
Brian Zanti <brianzanti@gmail.com>
2017-05-11 13:50:35 -0400

    app.component uses resume service and displays contents

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import '../assets/css/styles.css';
+
+import { ResumeService } from './service/resume/resume.service';
+import { Resume } from './model/resume/resume';
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit{ 
+
+	resume : Resume;
+
+	title : String = "<h2>placeholder title</h2>";
+
+	constructor(private resumeService: ResumeService){}
+
+	ngOnInit(): void {
+		this.resumeService.getResume(1).then(
+			resume => {
+				this.resume = resume;				
+			}
+		);
+	}
+
+}
