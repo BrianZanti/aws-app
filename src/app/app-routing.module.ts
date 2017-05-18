cbc4f1aa9bb8444e080deea6785b54a72c941085
Brian Zanti <brianzanti@gmail.com>
2017-05-18 13:13:13 -0400

    can now route to resume component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './component/login/login.component';
+import { ResumeComponent } from './component/resume/resume.component';
 
 const routes: Routes = [  
   { path: 'login', component: LoginComponent },  
+  { path: 'resume', component: ResumeComponent }
 ];
 
 @NgModule({
