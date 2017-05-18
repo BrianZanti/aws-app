89cac31bc44abb0ed97b229324c7655bb74eac23
Brian Zanti <brianzanti@gmail.com>
2017-05-18 13:15:42 -0400

    moved resume display to its own component. Moved css styling to local rather than global sheet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
-
-import '../assets/css/styles.css';
-
-import { ResumeService } from './service/resume/resume.service';
-import { Resume } from './model/resume/resume';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
-export class AppComponent implements OnInit{ 
-
-	resume : Resume;
-	content : SafeHtml;
-	title : String = "<h2>placeholder title</h2>";
-
-	constructor(private resumeService: ResumeService,
-				private sanitizer: DomSanitizer,
-				){}
-
-	ngOnInit(): void {
-		this.resumeService.getResume(1).then(
-			resume => {
-				this.resume = resume;				
-				this.content = this.sanitizer.bypassSecurityTrustHtml(String(resume.content));
-			}
-		);
-
-	}
-
-}
+export class AppComponent {}
