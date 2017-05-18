5da0c018f633b543a368af02cf6050913ba153be
Brian Zanti <brianzanti@gmail.com>
2017-05-18 12:23:47 -0400

    can route to login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { FormsModule } from '@angular/forms';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './component/login/login.component';
 
@@ -18,6 +20,7 @@ import { AuthService } from './service/auth/auth.service';
     BrowserModule,
     HttpModule,
     FormsModule,
+    AppRoutingModule,
     InMemoryWebApiModule.forRoot(InMemoryDataService),    
   ],
   declarations: [
